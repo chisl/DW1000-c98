d0abe846485688ad2813af9755afe149568872ff
chisl <info@chisl.io>
2017-12-27 18:11:57 +0100

    Initial commit

---
+++ DW1000.c
@@ -0,0 +1,9 @@
+/*
+ * name:        DW1000
+ * description: A fully integrated single chip Ultra Wideband (UWB) low-power low-cost transceiver IC for 2-way ranging or TDOA location systems.
+ * manuf:       Decawave
+ * version:     0.1
+ * url:         http://www.decawave.com/support/download/file/nojs/796
+ * date:        2017-02-07
+ */
+
